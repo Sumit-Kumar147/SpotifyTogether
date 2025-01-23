ed145dfbb97373fe81a5449d375dab6ee8d581ff
Sumit Kumar <sumitgurjardilpura@gmail.com>
2025-01-24 01:26:09 +0530

    newChahes

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,4 +1,6 @@
-CLIENT_ID = "08cb230a331944c7bfa503dfc73dd46c"
-CLIENT_SECRET = "f78338ef85e74cf2be80970a90406b69"
+CLIENT_ID = "901b6a7490ce4db18d4992309ae51426"
+# prev_client: 08cb230a331944c7bfa503dfc73dd46c 
+CLIENT_SECRET = "1f68cfe1a5854a43ae35cbda2e561f05"
+# prev_secret: f78338ef85e74cf2be80970a90406b69
 REDIRECT_URI = "https://spotifytogether.onrender.com/spotify/redirect" 
 # previous redirect url: http://127.0.0.1:8000/spotify/redirect
